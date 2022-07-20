a20865a885a5f34e2668e73aaad316bb7ada4417
ralph.jin <dotirh4351@gmail.com>
2022-07-19 23:08:16 -0700

    으앙제발

---
+++ let's c/HELLO WORLD/FF/hello world.c
@@ -0,0 +1,9 @@
+//
+//  hello world.c
+//  FF
+//
+//  Created by ㅇㅇ on 2022/07/19.
+//
+
+#include "hello world.h"
+__APPLE_API_OBSOLETE
---
+++ let's c/HELLO WORLD/FF/hello world.h
@@ -0,0 +1,13 @@
+//
+//  hello world.h
+//  FF
+//
+//  Created by ㅇㅇ on 2022/07/19.
+//
+
+#ifndef hello_world_h
+#define hello_world_h
+
+#include <stdio.h>
+
+#endif /* hello_world_h */
