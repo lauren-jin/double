8f8311e732c8d5ab18f9e7e5bbb572671fdaeeff
ralph.jin <dotirh4351@gmail.com>
2022-07-19 22:55:35 -0700

    4th test

---
+++ let's c/HELLO WORLD/FF/FF/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  FF
+//
+//  Created by ㅇㅇ on 2022/07/19.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
